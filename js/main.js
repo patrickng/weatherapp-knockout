9b80ce2a0cd808be6a12214303f20dca8eca92dc
Patrick Ng <ckipel@gmail.com>
2013-08-16 18:33:03 -0400

    adding styling and formatting to app

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 function HourlyEntry(time, temperature, humidity, condition, condition_icon_url) {
 	var self = this;
-	self.time = ko.observable(time);
+	self.time = ko.observable(time.civil);
+	self.formattedDate = ko.computed(function() {
+		return time.month_name + " " + time.mday;
+	});
 	self.temperature = ko.observable(temperature);
 	self.humidity = ko.observable(humidity);
 	self.condition = ko.observable(condition);
 	self.condition_url = ko.observable(condition_icon_url);
+	self.shouldShowDay = ko.computed(function() {
+		if ((time.hour == "23") || (time.hour == "0")) {
+			return true;
+		}
+	});
 }
 
 function TenDayEntry(time, high, low, condition, condition_icon_url) {
@@ -18,33 +26,29 @@ function TenDayEntry(time, high, low, condition, condition_icon_url) {
 
 function Location(name) {
 	var self = this;
+
 	self.name = name;
+	self.hourlyForecast = ko.observableArray();
+	self.tenDayForecast = ko.observableArray();
 	self.city = function() {
 		return self.name.split(',')[0].replace(/\s+/g, '_');
 	};
 	self.state = function() {
 		return self.name.split(',')[1].replace(/\s+/g, '');
 	};
-	self.hourlyForecast = ko.observableArray();
-	self.tenDayForecast = ko.observableArray();
-	self.displayType = ko.observable();
-	self.displayType.subscribe(function(newValue) {
-		console.log(newValue);
-		console.log('display type change');
-	});
 	self.loadData = function() {
 		var endpointURLs = {
-			hourlyForecast: "http://api.wunderground.com/api/420cc13d75d8f243/hourly/q/"+self.state()+"/"+self.city()+".json",
-			tenDayForecast: "http://api.wunderground.com/api/420cc13d75d8f243/forecast10day/q/"+self.state()+"/"+self.city()+".json"
-			// hourlyForecast: "/js/hourly.json",
-			// tenDayForecast: "/js/10day.json"
+			// hourlyForecast: "http://api.wunderground.com/api/420cc13d75d8f243/hourly/q/"+self.state()+"/"+self.city()+".json?callback=?",
+			// tenDayForecast: "http://api.wunderground.com/api/420cc13d75d8f243/forecast10day/q/"+self.state()+"/"+self.city()+".json?callback=?"
+			hourlyForecast: "/js/hourly.json",
+			tenDayForecast: "/js/10day.json"
 		};
 
-		$.getJSON(endpointURLs.hourlyForecast + "?callback=?", function(data){
+		$.getJSON(endpointURLs.hourlyForecast, function(data){
 			ko.utils.arrayMap(data.hourly_forecast, function(item) {
 				self.hourlyForecast.push(
 					new HourlyEntry(
-						item.FCTTIME.pretty, 
+						item.FCTTIME, 
 						item.temp.english + "F", 
 						item.humidity + "%",
 						item.condition, 
@@ -54,7 +58,7 @@ function Location(name) {
 			});
 		});
 
-		$.getJSON(endpointURLs.tenDayForecast + "?callback=?", function(data){
+		$.getJSON(endpointURLs.tenDayForecast, function(data){
 			ko.utils.arrayMap(data.forecast.simpleforecast.forecastday, function(item) {
 				self.tenDayForecast.push(
 					new TenDayEntry(
@@ -66,38 +70,20 @@ function Location(name) {
 					)
 				);
 			});
-		});		
-
-		// for(var key in endpointURLs) {
-		// 	if(endpointURLs.hasOwnProperty(key)) {
-		// 		$.getJSON(endpointURLs[key], function(data){
-
-		// 			self.update(data);
-		// 		});
-		// 	}
-		// }
+		});
 	};
-	
 }
 
 function DetailedLocationViewModel(location) {
 	var self = this;
 	self.newLocation = location;
 	self.newLocation.loadData();
+	self.displayType = ko.observable();
 }
 
 function AppViewModel() {
 	var self = this;
 	var locationExistsInArray = function(name, array) {
-		// var exists = false;
-
-		// for (var i = 0; i < array.length; i++) {
-		// 	if (array[i].name == name) {
-		// 		exists = true;
-		// 		break;
-		// 	}
-		// }
-
 		var exists = ko.utils.arrayFirst(array, function(location) {
 			return location.name == name;
 		});
@@ -105,9 +91,15 @@ function AppViewModel() {
 		return !!exists;
 	};
 
-	self.locationName = ko.observable();
-	self.locations = ko.observableArray([new Location("New York, NY"), new Location("San Francisco, CA")]);
 	self.currentSelectedLocation = ko.observable();
+	self.locationName = ko.observable();
+	self.locations = ko.observableArray([
+		new Location("New York, NY"), 
+		new Location("San Francisco, CA")
+	]);
+	self.displayLocationPanel = ko.computed(function() {
+		return self.currentSelectedLocation() ? "show" : "hide";
+	}, AppViewModel);
 
 	self.addLocation = function() {
 		if ((self.locationName() != "") && !locationExistsInArray(self.locationName(), self.locations())) {
@@ -115,7 +107,6 @@ function AppViewModel() {
 		}
 		self.locationName("");
 	};
-
 	self.selectLocation = function(location) {
 		self.currentSelectedLocation(new DetailedLocationViewModel(location));
 	};
