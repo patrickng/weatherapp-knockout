a0c2a7bb1d6729eca26476a9072a5a2622ad2c83
Patrick Ng <ckipel@gmail.com>
2013-08-30 17:32:54 -0400

    organizing the AppViewModel

--- js/app/AppViewModel.js
+++ js/app/AppViewModel.js
@@ -1,6 +1,13 @@
 define(['knockout', 'Location', 'DetailedLocationViewModel', 'LocationStorage'], function(ko, Location, DetailedLocationViewModel, locationStorage) {
 	function AppViewModel() {
 		var self = this;
+
+		self.currentSelectedLocation = ko.observable();
+		self.locationName = ko.observable();
+		self.displayLocationPanel = ko.computed(function() {
+			return self.currentSelectedLocation() ? "show" : "hide";
+		}, AppViewModel);
+
 		var locationExistsInArray = function(name, array) {
 			var exists = ko.utils.arrayFirst(array, function(location) {
 				return location.name == name;
@@ -9,9 +16,6 @@ define(['knockout', 'Location', 'DetailedLocationViewModel', 'LocationStorage'],
 			return !!exists;
 		};
 
-		self.currentSelectedLocation = ko.observable();
-		self.locationName = ko.observable();
-
 		var stripLocationNames = function(locations) {
 			var tempLocationArray = [];
 			ko.utils.arrayForEach(locations, function(location) {
@@ -19,6 +23,23 @@ define(['knockout', 'Location', 'DetailedLocationViewModel', 'LocationStorage'],
 				tempLocationArray.push(obj);
 			});
 			return tempLocationArray;
+		};
+
+		self.addLocation = function() {
+			if ((self.locationName() != "") && !locationExistsInArray(self.locationName(), self.locations())) {
+				self.locations.push(new Location(self.locationName()));
+				locationStorage.set(stripLocationNames(self.locations()));
+			}
+			self.locationName("");
+		};
+		
+		self.selectLocation = function(location) {
+			self.currentSelectedLocation(new DetailedLocationViewModel(location));
+		};
+
+		self.removeLocation = function(location) {
+			self.locations.remove(location);
+			locationStorage.set(stripLocationNames(self.locations()));
 		};
 
 		if ((locationStorage.get() == null) || (locationStorage.get() == undefined) || (locationStorage.get() == "")) {
@@ -33,25 +54,6 @@ define(['knockout', 'Location', 'DetailedLocationViewModel', 'LocationStorage'],
 				self.locations.push(new Location(location.name));
 			});
 		}
-
-		self.displayLocationPanel = ko.computed(function() {
-			return self.currentSelectedLocation() ? "show" : "hide";
-		}, AppViewModel);
-
-		self.addLocation = function() {
-			if ((self.locationName() != "") && !locationExistsInArray(self.locationName(), self.locations())) {
-				self.locations.push(new Location(self.locationName()));
-				locationStorage.set(stripLocationNames(self.locations()));
-			}
-			self.locationName("");
-		};
-		self.selectLocation = function(location) {
-			self.currentSelectedLocation(new DetailedLocationViewModel(location));
-		};
-		self.removeLocation = function(location) {
-			self.locations.remove(location);
-			locationStorage.set(stripLocationNames(self.locations()));
-		};
 	}
 
 	return AppViewModel;
